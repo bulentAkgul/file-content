8d17acf90c9b5a1f5d44c6b41e5084a71f34c361
Bulent Akgul <bulent.akgul@me.com>
2022-05-27 09:38:49 +0300

    file/folder creator display feedback on terminal

--- src/Functions/MakeFile.php
+++ src/Functions/MakeFile.php
@@ -5,6 +5,7 @@ namespace Bakgul\FileContent\Functions;
 use Bakgul\Kernel\Helpers\Path;
 use Bakgul\Kernel\Tasks\MutateStub;
 use Bakgul\FileHistory\Services\LogServices\ForUndoingLogService;
+use Bakgul\Kernel\Functions\DumpFeedback;
 use Illuminate\Filesystem\Filesystem;
 
 class MakeFile
@@ -14,6 +15,8 @@ class MakeFile
         $file = Path::glue([$request['attr']['path'], $request['attr']['file']]);
 
         (new Filesystem)->put($file, MutateStub::get($request));
+
+        DumpFeedback::_($file, 'file');
 
         ForUndoingLogService::set($file, false, true);
     }
